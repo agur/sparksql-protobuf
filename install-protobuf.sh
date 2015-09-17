3904f20f45415e6d9550def3df1cbd0e8cb4be47
Amir Gur <Amir Gur amir_gur@apple.com>
2015-09-16 18:12:25 -0700

    #1 now it works out of the box on mac os.  Just using user space.

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -2,27 +2,21 @@
 
 set -ex
 
-die() {
-    echo "$@" >&2
-    exit 1
+PROTOBUF_VERSION_DEFAULT=2.6.1
+[ "$PROTOBUF_VERSION" ] || {
+    echo PROTOBUF_VERSION env var is undefined, using the current \(9/2015\) latest released $PROTOBUF_VERSION_DEFAULT as default.
+    PROTOBUF_VERSION=2.6.1
 }
 
-test -n "$PROTOBUF_VERSION" || die "PROTOBUF_VERSION env var is undefined"
-
 case "$PROTOBUF_VERSION" in
-2*)
-    basename=protobuf-$PROTOBUF_VERSION
-    ;;
-3*)
-    basename=protobuf-cpp-$PROTOBUF_VERSION
-    ;;
-*)
-    die "unknown protobuf version: $PROTOBUF_VERSION"
-    ;;
+    2*) basename=protobuf-$PROTOBUF_VERSION ;;
+    3*) basename=protobuf-cpp-$PROTOBUF_VERSION ;;
+     *) die "unknown protobuf version: $PROTOBUF_VERSION" ;;
 esac
 
 curl -sL https://github.com/google/protobuf/releases/download/v$PROTOBUF_VERSION/$basename.tar.gz | tar zx
-
 cd protobuf-$PROTOBUF_VERSION
-
-./configure --prefix=/home/travis && make -j2 && make install
+# TODO: Review - was /home/travis.  /Users/ is used for home dirs on a mac, and trying user space to avoid sudoing.
+dir=~/travis
+mkdir -p $dir
+./configure --prefix=$dir && make -j2 && make install
